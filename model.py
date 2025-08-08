097444ade578ab4a23826791b615e9db0d2a3b46
wroblewskigk <wroblewskigk@proton.me>
2025-08-08 19:16:13 +0200

    Implement MultiHeadAttention class

--- model.py
+++ model.py
@@ -76,3 +76,80 @@ class FeedForward(nn.Module):
 
     def forward(self, x):
         return self.linear2(self.dropout(torch.relu(self.linear1(x))))
+
+
+class MultiHeadAttention(nn.Module):
+    def __init__(self, dmodel: int, num_heads: int, dropout: float) -> None:
+        super().__init__()
+        self.attention_scores = None  #########################################################################DIFFERENT
+        self.dmodel = dmodel
+        self.num_heads = num_heads
+        assert num_heads % dmodel == 0, "num_heads must be divisible by dmodel"
+        self.num_heads = (
+            num_heads / dmodel
+        )  ####################################################################################################DIFFERENT
+
+        self.wq = nn.Linear(dmodel, dmodel)
+        self.wk = nn.Linear(dmodel, dmodel)
+        self.wv = nn.Linear(dmodel, dmodel)
+
+        self.wo = nn.Linear(dmodel, dmodel)
+        self.dropout = nn.Dropout(dropout)
+
+    @staticmethod
+    def attention(query, key, value, mask, dropout: nn.Dropout):
+        dk = query.shape[-1]
+
+        attention_scores = (query @ key.transpose(-2, -1)) / math.sqrt(dk)
+        if mask is not None:
+            attention_scores.masked_fill_(mask == 0, -math.inf)
+        attention_scores = attention_scores.softmax(dim=-1)
+        if dropout is not None:
+            attention_scores = dropout(attention_scores)
+
+        return (attention_scores @ value), attention_scores
+
+    def forward(self, q, k, v, mask):
+        query = self.wq(q)
+        key = self.wk(k)
+        value = self.wv(v)
+
+        """
+        Q, K, V transpose shape goes like this: 
+        
+        [batch, sequence_length, dmodel] -> 
+        [batch, sequence_length, num_heads, dk] -> 
+        [batch, num_heads, dmodel]
+        """
+        query = query.view(
+            query.shape[0],
+            query.shape[1],
+            self.num_heads,
+            self.dk,
+        ).transpose(1, 2)
+
+        key = key.view(
+            key.shape[0],
+            key.shape[1],
+            self.num_heads,
+            self.dk,
+        ).transpose(1, 2)
+
+        value = value.view(
+            value.shape[0],
+            value.shape[1],
+            self.num_heads,
+            self.dk,
+        ).transpose(1, 2)
+
+        x, self.attention_scores = MultiHeadAttention.attention(
+            query, key, value, mask, self.dropout
+        )
+
+        x = (
+            x.transpose(1, 2)
+            .contiguous()
+            .view(x.shape[0], -1, self.num_heads * self.dk)
+        )
+
+        return self.wo(x)
