f24817efbb05d1cca278791cd7e8a5f323641ba7
wroblewskigk <wroblewskigk@proton.me>
2025-08-08 20:27:42 +0200

    Implement Decoder class

--- model.py
+++ model.py
@@ -227,3 +227,17 @@ class DecoderBlock(nn.Module):
         )
         x = self.residual_connection[2](x, self.feedforward)
         return x
+
+
+class Decoder(nn.Module):
+    def __init__(self, dmodel, layers: nn.ModuleList):
+        super().__init__()
+        self.layers = layers
+        self.norm = LayerNormalization(
+            dmodel
+        )  ####################################################################################################DIFFERENT
+
+    def forward(self, x, encoder_output, src_mask, target_mask):
+        for layer in self.layers:
+            x = layer(x, encoder_output, src_mask, target_mask)
+        return self.norm(x)
